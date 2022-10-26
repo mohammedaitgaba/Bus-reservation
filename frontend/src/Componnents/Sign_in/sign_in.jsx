1d72045569d8a6d4b4144c27be5b962ed16de173
mohammedaitgaba <medgaba237@gmail.com>
2022-10-26 17:33:42 +0100

    add sign_up form

--- frontend/src/Componnents/Sign_in/sign_in.jsx
+++ frontend/src/Componnents/Sign_in/sign_in.jsx
@@ -1,10 +1,44 @@
 import {useState}  from "react";
 import './sign_in.css'
 const Sign_in = ({Open,Close})=>{
-    // const [sign_in,setSign_in] = useState(false)
-    console.log("in sign in ",Open);
+    const [IsRegister,setRegister] = useState(false)
+    const ToRegister =()=>{
+        setRegister(true)
+    }
+    const ToSign_in=()=>{ 
+        setRegister(false)
+    }
+
     if (!Open) return null
-    else{
+    if (IsRegister) {
+        return(
+            <section className="Sign_in_holder">
+            <form className="sign_in_form Register_form">
+                <div className="title">
+                    Registration
+                </div>
+                <div className='Close'>  
+                    <i className="fal fa-times-circle"  onClick={Close}></i>
+                </div>
+                <div className="form_body Registration">
+                    <input type="text" placeholder="Nom" required/>
+                    <input type="text" placeholder="Prenom" required/>
+                    <input type="number" placeholder="N°tele"  required/>
+                    <input type="email"  placeholder="Email" required/>
+                    <input type="password" placeholder="Password" required/>
+                    <input type="password" placeholder="Password_confirmation" required/>
+                </div>
+                <div className="not_registred"onClick={ToSign_in}>
+                    déjà membre ! connectez-vous
+                </div>
+                <div className="form_submit">
+                    <button type="button"> Cree compte </button>
+                </div>
+            </form>
+        </section>
+        )
+    }else{
+
         return(
             <section className="Sign_in_holder">
                 <form className="sign_in_form">
@@ -12,13 +46,13 @@ const Sign_in = ({Open,Close})=>{
                         Authentification
                     </div>
                     <div className='Close'>  
-                        <i class="fal fa-times-circle"  onClick={Close}></i>
+                        <i className="fal fa-times-circle"  onClick={Close}></i>
                     </div>
                     <div className="form_body">
                         <input type="email" placeholder="email@exemple.xx" required/>
                         <input type="password" placeholder="Password" required/>
                     </div>
-                    <div className="not_registred">
+                    <div className="not_registred" onClick={ToRegister}>
                         Nouveau membre ! créer un compte
                     </div>
                     <div className="form_submit">
@@ -28,7 +62,7 @@ const Sign_in = ({Open,Close})=>{
             </section>
         )
     }
+    }
 
-}
 
 export default Sign_in;
